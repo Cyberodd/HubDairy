68383b1a8db5271895b86e43ccc74dcc62c668c4
Evans Shango <lutatwaevans@gmail.com>
2020-01-07 22:35:52 +0300

    initial commit

---
+++ app/src/main/java/com/hub/dairy/MainActivity.java
@@ -0,0 +1,15 @@
+
+package com.hub.dairy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
